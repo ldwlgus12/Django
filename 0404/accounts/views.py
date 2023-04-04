3080868a89331b55867c93829c9bbce8eafc8e9b
ldwlgus12 <ldwlgus12@naver.com>
2023-04-04 17:14:13 +0900

    0404_django&practice

---
+++ 0404/accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+
+# 1. 로그인 페이지 (GET)
+# 2. 로그인 실행 페이지 (POST)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
