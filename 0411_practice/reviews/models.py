bc9b462f34f6e3dd8f74ad9d1ee7c45c15f723cf
ldwlgus12 <ldwlgus12@naver.com>
2023-04-11 19:27:49 +0900

    0411_practice

---
+++ 0411_practice/reviews/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    movie = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
